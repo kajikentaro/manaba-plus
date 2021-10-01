71ce1f2b4eabf125a45ccac7bcdff357a8347ddf
kajikentaro <k.kentarou0412@gmail.com>
2021-10-01 15:56:33 +0900

    fix timeout button layout

--- src/timeout.ts
+++ src/timeout.ts
@@ -1,8 +1,9 @@
-console.log("hello")
-let go_home_btn = document.createElement("a");
-go_home_btn.id = "go-home-btn";
+let go_home_btn = document.createElement("button");
 go_home_btn.innerHTML = "ログイン画面へ";
-go_home_btn.href = "https://room.chuo-u.ac.jp/ct/home"
+go_home_btn.classList.add("button");
+go_home_btn.onclick = () => {
+	window.location.href = "https://room.chuo-u.ac.jp/ct/home";
+}
 
 let center_div = document.createElement("div");
 center_div.style.textAlign = "center";
