06a99b9ce5f3f64f82a8be3ba42cd098d4a491c5
kajikentaro <k.kentarou0412@gmail.com>
2022-04-10 14:03:54 +0000

    Refactor: chromeダウンロード関数のモジュール化

---
+++ src/module/DownloadFile.ts
@@ -0,0 +1,32 @@
+import * as PathModule from "path";
+import { FileInfo } from "./type";
+
+const downloadFile: (file: FileInfo, onDownloadStart: (downloadId: number) => void) => Promise<void> = async (file, onDownloadStart) => {
+  const fileName = decodeURI(file.url.match(".+/(.+?)([?#;].*)?$")[1]);
+  const filePath = PathModule.join("Manaba", file.courseName.replace("/", "-"), file.contentName.replace("/", "-"), fileName.replace("/", "-"));
+  const downloadId = await chrome.downloads.download({ url: file.url, filename: filePath, saveAs: false });
+
+  if (typeof downloadId === "undefined") {
+    return Promise.reject(new Error(""));
+  }
+  onDownloadStart(downloadId);
+  await waitForComplete(downloadId);
+};
+
+const waitForComplete: (downloadId: number) => Promise<void> = (downloadId) => {
+  return new Promise((resolve, reject) => {
+    const callback: (downloadDelta: chrome.downloads.DownloadDelta) => void = (downloadDelta) => {
+      if (downloadId !== downloadDelta.id) return;
+      chrome.downloads.onChanged.removeListener(callback);
+      if (downloadDelta.state) {
+        if (downloadDelta.state.current === "interrupted") reject(downloadDelta.error || new Error());
+        if (downloadDelta.state.current === "complete") resolve();
+      } else {
+        reject(new Error());
+      }
+    };
+    chrome.downloads.onChanged.addListener(callback);
+  });
+};
+
+export default downloadFile;
