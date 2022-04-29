f093b8c78b160190079d4c69598807cbd0e6cabf
hotari24tools <hotari24tools@gmail.com>
2022-04-29 20:07:05 +0900

    add: to hide some elements

--- src/home.ts
+++ src/home.ts
@@ -1,14 +1,28 @@
-import { InfinityDate, URL_HOME, ENABLE_INSERT_MP, HIDED_ASSIGNHMENT } from "./module/const";
+import { InfinityDate, URL_HOME, ENABLE_INSERT_MP, HIDDEN_ASSIGNMENTS, STORAGE_KEY_KIKUZOU, STORAGE_KEY_SMARTPHONE, STORAGE_KEY_ASSIGNMENT_HISTORY, STORAGE_KEY_SEARCH_SYLLABUS, STORAGE_KEY_STYLE_PERMISSION } from "./module/const";
 import { HTMLInputEvent, AssignmentMember } from "./module/type";
 import Assignment from "./module/Assignment";
 import AssignmentViewer from "./module/AssignmentViewer";
+import * as Storage from "./module/storage";
 
-let startMpDone = false;
+let startMpDone = false; // 未提出課題一覧が表示されているかどうか
 
+// 初期化する。
 const init = async () => {
-  const enableInsertMp = await isEnableInsertMp();
-  if (enableInsertMp === false) return;
   await insertMpButton();
+  await overwriteStyles();
+  await hideElements();
+};
+
+// manaba上部にManaba Plusのコンテンツを挿入する。
+const insertMpButton = async () => {
+  const enableInsertMp = await Storage.getBoolean(ENABLE_INSERT_MP);
+  if (enableInsertMp === false) return;
+
+  const mark = document.getElementsByClassName("contentbody-left")[0];
+  mark.insertAdjacentHTML("afterbegin", await (await fetch(chrome.runtime.getURL("insert.html"))).text());
+  document.getElementById("show-assignment").onclick = () => {
+    startMp();
+  };
   document.getElementById("download-content").onclick = () => {
     if (confirm("コースコンテンツをPCにまとめてダウンロードします。続行しますか？")) {
       window.open(chrome.runtime.getURL("download-progress.html"));
@@ -19,6 +33,76 @@ const init = async () => {
   };
 };
 
+// manabaのHTML要素のstyleを上書きする。
+const overwriteStyles = async () => {
+  const style_permission = await Storage.getBoolean(STORAGE_KEY_STYLE_PERMISSION);
+  if (style_permission !== true) {
+    return;
+  }
+
+  const contentbody_left = document.getElementsByClassName("contentbody-left")[0] as HTMLElement;
+  contentbody_left.style.width = "671px";
+  contentbody_left.style.paddingRight = "15px";
+
+  document.getElementById("courselistweekly").style.paddingRight = "0px";
+  const infolistHeaders = document.getElementsByClassName("my-infolist-header") as HTMLCollectionOf<HTMLElement>;
+  Array.from(infolistHeaders).forEach((element) => {
+    element.style.backgroundSize = "100% 100%";
+  });
+
+  const tableHeaders = document.querySelectorAll<HTMLElement>(".courselist th");
+  tableHeaders[0].setAttribute("width", "auto");
+  tableHeaders[1].setAttribute("width", "50px");
+  tableHeaders[2].setAttribute("width", "50px");
+  tableHeaders[3].setAttribute("width", "20%");
+}
+
+// 要素を非表示にする。
+const hideElements = async () => {
+  const hide = (className: string) => {
+    const target = document.getElementsByClassName(className)[0] as HTMLElement;
+    target.setAttribute("hidden", "");
+  }
+
+  const style_permission = await Storage.getBoolean(STORAGE_KEY_STYLE_PERMISSION);
+  const search_syllabus = await Storage.getBoolean(STORAGE_KEY_SEARCH_SYLLABUS);
+  const assignment_history = await Storage.getBoolean(STORAGE_KEY_ASSIGNMENT_HISTORY);
+  const smartphone = await Storage.getBoolean(STORAGE_KEY_SMARTPHONE);
+  const kikuzou = await Storage.getBoolean(STORAGE_KEY_KIKUZOU);
+
+  if (search_syllabus) {
+    hide("my-infolist-searchall");
+  }
+
+  if (assignment_history) {
+    hide("my-infolist-event");
+  }
+
+  if (smartphone) {
+    const elements = document.getElementsByClassName("my-infolist-tips") as HTMLCollectionOf<HTMLElement>;
+    const target = Array.from(elements).find(element => !element.classList.contains("my-infolist-kikuzou"));
+    target.setAttribute("hidden", "");
+  }
+
+  if (kikuzou) {
+    hide("my-infolist-kikuzou");
+  }
+
+  if (style_permission && [search_syllabus, assignment_history, smartphone, kikuzou].every(x => x)) {
+    const contentbody_left = document.getElementsByClassName("contentbody-left")[0] as HTMLElement;
+    contentbody_left.style.width = "916px";
+
+    const my_infolist_mycourses = document.getElementsByClassName("my-infolist-mycourses")[0] as HTMLElement;
+    my_infolist_mycourses.style.paddingLeft = "10px";
+
+    const elements = document.getElementsByClassName("course") as HTMLCollectionOf<HTMLElement>;
+    Array.from(elements).forEach(element => {
+      element.style.height = "60px";
+    });
+  }
+}
+
+// 未提出課題一覧を表示する。
 const startMp = async () => {
   // prevent over two times button click
   if (startMpDone) return;
@@ -46,35 +130,7 @@ const startMp = async () => {
   viewer.repaint();
 };
 
-const getCourseURLs = () => {
-  const manabaCourseDOMs = document.querySelectorAll<HTMLAnchorElement>(".course-cell a:first-child");
-  const courseURLs = [] as string[];
-  manabaCourseDOMs.forEach((manabaCourseDOM) => {
-    courseURLs.push(manabaCourseDOM.href);
-  });
-  return courseURLs;
-};
-
-const isEnableInsertMp = async () => {
-  const res = await new Promise((resolve) => {
-    chrome.storage.local.get([ENABLE_INSERT_MP], function (result) {
-      if (result[ENABLE_INSERT_MP] === undefined) resolve(true);
-      resolve(result[ENABLE_INSERT_MP]);
-    });
-  });
-  return res;
-};
-
-const fetchHided = async () => {
-  const res = await new Promise((resolve) => {
-    chrome.storage.sync.get([HIDED_ASSIGNHMENT], function (result) {
-      if (!result[HIDED_ASSIGNHMENT]) resolve([]);
-      resolve(result[HIDED_ASSIGNHMENT]);
-    });
-  });
-  return res as string[];
-};
-
+// manabaの未提出課題一覧から課題のリストを取得する。
 const fetchSummaries = async () => {
   const docParser = (doc: Document) => {
     const clipStr = (element: HTMLElement) => {
@@ -114,12 +170,25 @@ const fetchSummaries = async () => {
   return assignments;
 };
 
-const insertMpButton = async () => {
-  const mark = document.getElementsByClassName("contentbody-left")[0];
-  mark.insertAdjacentHTML("afterbegin", await (await fetch(chrome.runtime.getURL("insert.html"))).text());
-  document.getElementById("show-assignment").onclick = () => {
-    startMp();
-  };
+// 非表示の課題リストを取得する。
+const fetchHided = async () => {
+  const res = await new Promise((resolve) => {
+    chrome.storage.sync.get([HIDDEN_ASSIGNMENTS], function (result) {
+      if (!result[HIDDEN_ASSIGNMENTS]) resolve([]);
+      resolve(result[HIDDEN_ASSIGNMENTS]);
+    });
+  });
+  return res as string[];
+};
+
+// 各コースのURLを取得する。
+const getCourseURLs = () => {
+  const manabaCourseDOMs = document.querySelectorAll<HTMLAnchorElement>(".course-cell a:first-child");
+  const courseURLs = [] as string[];
+  manabaCourseDOMs.forEach((manabaCourseDOM) => {
+    courseURLs.push(manabaCourseDOM.href);
+  });
+  return courseURLs;
 };
 
 init();
--- src/options.ts
+++ src/options.ts
@@ -1,5 +1,6 @@
-import { DOWNLOAD_LIST, ENABLE_INSERT_MP, HIDED_ASSIGNHMENT } from "./module/const";
-import { HTMLInputEvent } from "./module/type";
+import { DOWNLOAD_LIST, STORAGE_KEY_ASSIGNMENT_HISTORY, ENABLE_INSERT_MP, STORAGE_KEY_KIKUZOU, STORAGE_KEY_SEARCH_SYLLABUS, STORAGE_KEY_SMARTPHONE, STORAGE_KEY_STYLE_PERMISSION, HIDDEN_ASSIGNMENTS } from "./module/const";
+import { BooleanStorageKey, HTMLInputEvent } from "./module/type";
+import * as Storage from "./module/storage";
 
 document.getElementById("delete-download-history").addEventListener("click", () => {
   if (!confirm("よろしいですか？")) return;
@@ -9,31 +10,33 @@ document.getElementById("delete-download-history").addEventListener("click", () 
 });
 document.getElementById("reset-hidden-ass").addEventListener("click", () => {
   if (!confirm("よろしいですか？")) return;
-  chrome.storage.sync.set({ [HIDED_ASSIGNHMENT]: [] }, () => {
+  chrome.storage.sync.set({ [HIDDEN_ASSIGNMENTS]: [] }, () => {
     alert("完了");
   });
 });
 document.getElementById("contents-download").addEventListener("click", () => {
   window.open("download-progress.html");
 });
-const isEnableInsertMp = async () => {
-  const res = await new Promise((resolve) => {
-    chrome.storage.local.get([ENABLE_INSERT_MP], function (result) {
-      if (result[ENABLE_INSERT_MP] === undefined) resolve(true);
-      resolve(result[ENABLE_INSERT_MP]);
-    });
-  });
-  return res as boolean;
-};
+
 const initToggleCheck = async () => {
-  const enableInsertMp = await isEnableInsertMp();
-  document.querySelector<HTMLInputElement>("input#toggle-hide").checked = !enableInsertMp;
-  document.querySelector<HTMLInputElement>("input#toggle-hide").onchange = (v: HTMLInputEvent) => {
-    if (v.target.checked) {
-      chrome.storage.local.set({ [ENABLE_INSERT_MP]: false });
-    } else {
-      chrome.storage.local.set({ [ENABLE_INSERT_MP]: true });
-    }
-  };
+  const enableInsertMp = await Storage.getBoolean(ENABLE_INSERT_MP);
+  const input = document.querySelector<HTMLInputElement>("input#toggle-hide");
+  input.checked = !enableInsertMp;
+  input.onchange = (e: HTMLInputEvent) => Storage.setBoolean(ENABLE_INSERT_MP, !e.target.checked);
 };
+
+const bindBoolean = async (key: BooleanStorageKey) => {
+  const input = document.getElementById(`${key.name}_checkbox`) as HTMLInputElement;
+  input.checked = await Storage.getBoolean(key);
+  input.onchange = (e: HTMLInputEvent) => {
+    Storage.setBoolean(key, e.target.checked);
+  }
+}
+
 initToggleCheck();
+
+bindBoolean(STORAGE_KEY_STYLE_PERMISSION);
+bindBoolean(STORAGE_KEY_SEARCH_SYLLABUS);
+bindBoolean(STORAGE_KEY_ASSIGNMENT_HISTORY);
+bindBoolean(STORAGE_KEY_SMARTPHONE);
+bindBoolean(STORAGE_KEY_KIKUZOU);
