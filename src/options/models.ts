d6f0e8cb6ea26d9611aa1ad4190ae57b78983403
HotariTobu <hotari24tools@gmail.com>
2023-02-25 02:14:30 +0900

    remove: extra comments

--- src/options/models.ts
+++ src/options/models.ts
@@ -50,37 +50,3 @@ export default async () => {
   console.log(options)
   return options
 }
-
-// import {
-//   DOWNLOAD_LIST,
-//   HIDDEN_ASSIGNMENTS,
-//   STORAGE_KEY_ASSIGNMENT_HISTORY,
-//   STORAGE_KEY_KIKUZOU,
-//   STORAGE_KEY_SEARCH_SYLLABUS,
-//   STORAGE_KEY_SMARTPHONE,
-//   STORAGE_KEY_STYLE_PERMISSION,
-//   STORAGE_KEY_TOP_MENU,
-// } from "./module/const"
-// import * as Storage from "./module/storage"
-// import { BooleanStorageKey, HTMLInputEvent } from "./module/type"
-
-// /**
-//  * Manaba Plus オプション画面のボタン動作を定義するプログラム
-//  */
-// document
-//   .getElementById("delete-download-history")
-//   .addEventListener("click", () => {
-//     if (!confirm("よろしいですか？")) return
-//     chrome.storage.local.set({ [DOWNLOAD_LIST]: [] }, () => {
-//       alert("完了")
-//     })
-//   })
-// document.getElementById("reset-hidden-ass").addEventListener("click", () => {
-//   if (!confirm("よろしいですか？")) return
-//   chrome.storage.sync.set({ [HIDDEN_ASSIGNMENTS]: [] }, () => {
-//     alert("完了")
-//   })
-// })
-// document.getElementById("contents-download").addEventListener("click", () => {
-//   window.open("download-progress.html")
-// })
