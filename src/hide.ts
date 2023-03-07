6ff1d673c3f31a0eb40b555dc9aea6cb8a5d243b
HotariTobu <hotari24tools@gmail.com>
2023-03-08 01:40:47 +0900

    remove: hide.ts

--- src/hide.ts
+++
@@ -1,7 +0,0 @@
-import './extension/htmlElement'
-
-export default function (selectors: string) {
-  document.querySelectorAll<HTMLElement>(selectors).forEach(function (element) {
-    element.shown(false)
-  })
-}
