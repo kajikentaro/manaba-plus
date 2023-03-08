3dcb6630840bda3c773a2aa39a7779fe859e3b6d
HotariTobu <hotari24tools@gmail.com>
2023-03-08 10:42:58 +0900

    update: to arrange with css

--- src/common/arrange.ts
+++ src/common/arrange.ts
@@ -1,17 +1,6 @@
 export default function () {
   // #region To show background image.
-  const orgheader = document.querySelector<HTMLElement>('#orgheader')
-  if (orgheader !== null) {
-    orgheader.style.backgroundColor = 'transparent'
-  }
-
-  const pagebody = document.querySelector<HTMLElement>('.pagebody')
-  if (pagebody !== null) {
-    pagebody.style.borderStyle = 'none'
-  }
   // #endregion
-
   // #region Make responsible.
-
   // #endregion
 }
