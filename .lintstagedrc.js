fc9f0ec7dc4abe44b70a10b55d4f51f4392de397
HotariTobu <hotari24tools@gmail.com>
2023-04-04 16:26:43 +0900

    fix: lint-staged configs

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+const { ESLint } = require('eslint')
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    })
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
+
+module.exports = {
+  '*.{js,ts,scss,md,pug}': 'prettier --check --loglevel=warn',
+  '*.{js,ts}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint --max-warnings=0 ${filesToLint}`]
+  },
+  '*.{css,scss}': 'stylelint --allow-empty-input',
+}
